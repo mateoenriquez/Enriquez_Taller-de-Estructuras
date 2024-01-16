a8159b4868269628e0e7095936f22c5999db97f4
Mateo Enriquez <mateo.enriquez@udla.edu.ec>
2024-01-16 15:21:13 -0500

    Obtener datos

--- main.c
+++ main.c
@@ -9,3 +9,28 @@ struct alumno
     float promedio;
 };
 
+void obtenerDatos(struct alumno *alumno);
+
+void obtenerDatos(struct alumno *alumno)
+{
+    printf("Ingrese datos para el alumno:\n");
+
+    printf("Matricula: ");
+    scanf("%d", &alumno->matricula);
+
+    fflush(stdin);  // Limpiar el búfer del teclado
+
+    printf("Nombre: ");
+    gets(alumno->nombre);
+
+    printf("Dirección: ");
+    gets(alumno->direccion);
+
+    printf("Carrera: ");
+    gets(alumno->carrera);
+
+    printf("Promedio: ");
+    scanf("%f", &alumno->promedio);
+}
+
+
